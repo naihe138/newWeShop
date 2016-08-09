50b3507cf28f020640f3688e09bc9aec427e85ac
naihe138 <370215230@qq.com>
2016-08-10 01:13:34 +0800

    首页banner的交口出调

--- js/index.js
+++ js/index.js
@@ -1,9 +1,51 @@
 $(function() {
-    var mySwiper = new Swiper('.swiper-container', {
-        loop: true
-        //其他设置
-    });
-    new xScroll({
-	    el:'#container'
-	});
+
+	// 整个项目的初始化函数	
+	function init(){
+		// 整个容器滚动
+		new xScroll({
+		    el:'#container'
+		});
+		// banner
+		banner.init();
+		
+	}
+	// Banner
+	var banner = (function(){
+		var bannerBox = $('#banner');
+		var nav = $('.banner-nav');
+		var tem = '';
+		var navTem = '';
+		function init(){
+			$.post('http://api.shopbymall.com/api/home/GetHomeBanner', function(res){
+				console.log(res);
+				if(res.returnstate == 200){
+					res.returnvalue.forEach(function(item, i){
+						console.log(item);
+						tem += '<div class="swiper-slide"><img src="'+item.ImgSrc+'"></div>';
+						navTem += '<span></span>';
+					})
+				}
+				bannerBox.append($(tem));
+				nav.append($(navTem));
+				nav.find('span').eq(0).addClass('active');
+				// 
+				setTimeout(function(){
+					var swiper = new Swiper('.swiper-container', {
+				        loop: true,
+				        onSlideChangeEnd: function(){
+				        	nav.find('span').eq(swiper.activeIndex-2).addClass('active').siblings('span').removeClass('active');
+				        }
+				    });
+				}, 30)
+				
+			})
+		}
+		return {
+			init: init
+		}
+	})();
+	
+	// 初始化的调用
+	init();
 })
